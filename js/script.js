8be3df398c030f0c52ffda809503ce0f43779373
Vitalii Ihnatenko <ignatenko.9999@gmail.com>
2017-06-12 17:12:03 +0300

    Remake icons and OUR WORKS

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ var slider_text_arr = [
 ];
 var timerId = false;
 var timeoutId = false;
+
 start_sliding();
 
 
@@ -132,6 +133,90 @@ function delete_image()
     }, 1000)
     return(true);
 }
+
+var numbers_photos = [3, 3, 4, 3];
+var categorys_photos = ["WebDesign", "GraphicDesign", "Photography", "Illustration"];
+var item_menu = ["WEB DESIGN", "GRAPHIC DESIGN", "PHOTOGRAPHY", "ILLUSTRATION"];
+
+recreateGaleryAll();
+
+function createNewPhoto(photo, photo_parent, category_photo, number_photo)
+{
+	var photo_next = photo.cloneNode(true);
+	photo_next.style.backgroundImage = "url(img/What_we_done_so_far/" + category_photo + "/" + number_photo + ".jpg";
+	photo_next.querySelector(".name_photo").innerText = "IMAGE" + number_photo;
+	photo_next.querySelector(".category_photo").innerText = category_photo;
+	photo_parent.appendChild(photo_next);
+}
+
+function recreateGaleryAll()
+{
+	var photo = document.querySelector(".photo");
+	var photo_parent = photo.parentNode;
+	deleteGalery();
+	for(var i = 0; i < 4; i++)
+	{
+		for(var j = 0; j < numbers_photos[i]; j++)
+		{
+			createNewPhoto(photo, photo_parent, categorys_photos[i], j);
+		}
+	}
+	
+	document.querySelector(".photo_menu ul li:first-child a").style.color = "#ffe600";
+}
+
+function recreateGalery(category_photo)
+{
+	var photo = document.querySelector(".photo");
+	var photo_parent = photo.parentNode;
+	deleteGalery();
+
+		for(var j = 0; j < numbers_photos[category_photo]; j++)
+		{
+			createNewPhoto(photo, photo_parent, categorys_photos[category_photo], j);
+		}
+}
+
+function deleteGalery()
+{
+	var photos = document.querySelectorAll(".photo");
+	for (var i = 0; i < photos.length; i++) {
+        photos[i].remove();
+    }
+}
+
+var galeryMenu = document.querySelector(".photo_menu ul");
+galeryMenu.onclick = function(event)
+	{
+		var a = this.querySelectorAll("a");
+		for(var i = 0; i < a.length; i++)
+		{
+			a[i].style.color = "#5e5e5e";
+		}
+
+		switch(event.target.innerText) 
+		{
+			case "ALL": 
+				recreateGaleryAll();
+				break;			
+			case item_menu[0]: 
+				recreateGalery(0);
+				break;			
+			case item_menu[1]: 
+				recreateGalery(1);
+				break;			
+			case item_menu[2]: 
+				recreateGalery(2);
+				break;
+			case item_menu[3]: 
+				recreateGalery(3);
+				break;
+		}
+		event.target.style.color = "#ffe600";
+	}
+
+
+
 
 
 
